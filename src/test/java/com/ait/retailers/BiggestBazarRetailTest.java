52d2f1171e5c96468ec6472d3c44f7cd6579acf3
fsusam <fsusam@gmail.com>
2020-02-26 23:18:08 +0000

    update java version in pom.xml

--- src/test/java/com/ait/retailers/BiggestBazarRetailTest.java
+++ src/test/java/com/ait/retailers/BiggestBazarRetailTest.java
@@ -2,6 +2,7 @@ package com.ait.retailers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -32,8 +33,13 @@ public class BiggestBazarRetailTest {
         when(inventory.getItemsExpireInAMonth()).thenReturn(new ArrayList<Item>(Arrays.asList(item)));
         when(inventory.itemsUpdated()).thenReturn(1);
         final int updatedItems = biggestBazarRetail.issueDiscountForItemsExpireIn30Days(0.1);
+
         verify(inventory).update(item, 90);
+
         verify(pas).announce(any(Offer.class));
+        // or
+        verify(pas).announce(isA(Offer.class));
+
         assertEquals(updatedItems, 1);
     }
 
@@ -41,11 +47,11 @@ public class BiggestBazarRetailTest {
     public void no_Item_qualifies_for_a_discount_if_the_discounted_price_is_less_than_the_base_price() {
         item = new Item("1111", "Item1", 100.0, 80.0);
         when(inventory.getItemsExpireInAMonth()).thenReturn(new ArrayList<Item>(Arrays.asList(item)));
-        when(inventory.itemsUpdated()).thenReturn(1);
+        when(inventory.itemsUpdated()).thenReturn(0);
         final int updatedItems = biggestBazarRetail.issueDiscountForItemsExpireIn30Days(0.3);
         verify(inventory, never()).update(any(Item.class), any(Double.class));
         verify(pas, never()).announce(any(Offer.class));
-        assertEquals(updatedItems, 1);
+        assertEquals(updatedItems, 0);
     }
 
     @Test
@@ -56,10 +62,10 @@ public class BiggestBazarRetailTest {
         items.add(new Item("1111", "Item1", 100.0, 50.0));
 
         when(inventory.getItemsExpireInAMonth()).thenReturn(items);
-        when(inventory.itemsUpdated()).thenReturn(3);
+        when(inventory.itemsUpdated()).thenReturn(2);
         final int updatedItems = biggestBazarRetail.issueDiscountForItemsExpireIn30Days(0.3);
         verify(inventory, times(2)).update(any(Item.class), any(Double.class));
         verify(pas, times(2)).announce(any(Offer.class));
-        assertEquals(updatedItems, 3);
+        assertEquals(updatedItems, 2);
     }
 }
